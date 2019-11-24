ec43b327a8bef162365a7fd5129bbb5b25d1145b
yhu66 <54507360+yhu66@users.noreply.github.com>
2019-11-24 04:49:45 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 function init(){
 //add your javascrip between these two lines of code
  
+  var input = document.getElementById('entryinput');
+  var button = document.getElementById('entrybutton');
+  var h2 = document.getElementById('textoutput');
+  
+  button.addEventListener('click', function(){
+  alert('Yixin: ' + text.value);
+});
 
+  button.addEventListener('click', function(){
+  h2.innerHTML = text.value;
+});
+
+}
 
 
 
